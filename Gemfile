985c159a8c008beda672c7eb70781ff65328239d
alexandrafren <alexandra.fren@gmail.com>
2019-04-12 13:41:16 -0700

    JSON data pulls into todocontainer

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-# gem 'jbuilder', '~> 2.5'
+ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
@@ -26,6 +26,9 @@ gem 'puma', '~> 3.11'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+# bundle exec rake doc:rails generates the API under doc/api.
+gem 'sdoc', '~> 0.4.0', group: :doc
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
@@ -35,6 +38,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'web-console', '~> 2.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- client/src/containers/TodosContainer.js
+++ client/src/containers/TodosContainer.js
@@ -6,22 +6,44 @@ import Todo from '../components/Todo'
 class TodosContainer extends Component {
 
   //renderTodos = () => this.props.todos.map((todo, i) => <Todo key={i} name={todo.name} />)
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+  }
 
+  componentDidMount(){
+    return fetch('api/todo')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      let todos = data.map((todo) => {
+        return(todo.name)
+      })
+      this.setState({todos: todos})
+      console.log(this.state.todos)
+    })
+  }
+  
   render() {
     return(
       <div>
         <TodoInput addTodo={this.props.addTodo} />
+        {this.state.todos}
       </div>
     )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    todos: state.todos
-  }
-}
+//const mapStateToProps = state => {
+  //return {
+    //todos: state.todos
+  //}
+//}
 
-const mapDispatchToProps = (dispatch) => ({addTodo: todo => dispatch({type: 'ADD_TODO', todo}) })
+//const mapDispatchToProps = (dispatch) => ({addTodo: todo => dispatch({type: 'ADD_TODO', todo}) })
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
+///xport default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
+
+export default TodosContainer;
