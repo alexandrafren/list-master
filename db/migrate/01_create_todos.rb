69214b4bae30535f9fad18b26110f65d54b8975c
alexandrafren <alexandra.fren@gmail.com>
2019-04-12 19:19:34 -0700

    create migrations for projects and update todos

--- db/migrate/01_create_todos.rb
+++ db/migrate/01_create_todos.rb
@@ -2,6 +2,9 @@ class CreateTodos < ActiveRecord::Migration[5.2]
     def change
         create_table :todos do |t|
             t.string :name
+            t.integer :project_id
+            t.integer :time_to_complete
+            t.integer :level_of_difficulty
         end
     end
 end
