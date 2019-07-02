ac56c178d448e2ec9b27f600b1e17b54cc992d2e
alexandrafren <alexandra.fren@gmail.com>
2019-07-01 22:12:38 -0700

    add header for project

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import TodosContainer from './containers/TodosContainer'
 import SuggestionContainer from './containers/SuggestionContainer'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="div-a">
+        <div className="head">
+          <Header />
+        </div>
+        <div>
           <SuggestionContainer />
         </div>
           <br />
-        <div className="div-a">
+        <div>
           <TodosContainer />
         </div>
       </div>
---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+
+  render() {
+    return(
+      <>
+        <h1>Defeat Decision Fatigue</h1>
+        <h3>The Task Manager That Makes Decisions For You</h3>
+      </>
+    )
+  }
+
+}
+
+export default Header;
