1d968310d469a6a3a240894a6f2c54969822fa92
alexandrafren <alexandra.fren@gmail.com>
2019-11-11 19:12:52 -0800

    save

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import TodosContainer from './containers/TodosContainer'
 import SuggestionContainer from './containers/SuggestionContainer'
 import Header from './components/Header'
-import CommentContainer from './containers/CommentContainer'
 
 class App extends Component {
   render() {
